bafde0003ef57c48f28f91cb34218626f99026cc
Matthew Horridge <matthew.horridge@stanford.edu>
2023-09-21 11:45:46 -0700

    Moved service interfaces and classes from protegeproject/webprotege-gh--issues

---
+++ src/main/java/edu/stanford/protege/issues/service/GitHubIssuesRepository.java
@@ -0,0 +1,10 @@
+package edu.stanford.protege.issues.service;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2023-09-15
+ */
+public interface GitHubIssuesRepository {
+
+}

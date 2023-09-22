d1e8874d162528253e890716ce66c28b1d2ef16e
Matthew Horridge <matthew.horridge@stanford.edu>
2023-09-21 18:07:54 -0700

    Adding OboId

---
+++ src/main/java/edu/stanford/protege/issues/service/OboId.java
@@ -0,0 +1,25 @@
+package edu.stanford.protege.issues.service;
+
+import com.fasterxml.jackson.annotation.JsonValue;
+
+import javax.annotation.Nonnull;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+public record OboId(@Nonnull String id) {
+
+    public OboId(@Nonnull String id) {
+        this.id = checkNotNull(id);
+    }
+
+    @Nonnull
+    public static OboId valueOf(@Nonnull String id) {
+        return new OboId(id);
+    }
+
+    @JsonValue
+    @Override
+    public String id() {
+        return id;
+    }
+}
---
+++ src/test/java/edu/stanford/protege/issues/service/OboIdTest.java
@@ -0,0 +1,72 @@
+package edu.stanford.protege.issues.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 2023-09-21
+ */
+@JsonTest
+@AutoConfigureJson
+public class OboIdTest {
+
+    protected static final String OBO_ID = "GO:1234567";
+
+    @Autowired
+    private JacksonTester<OboId> tester;
+
+    @Test
+    public void shouldCreateOboIdViaConstructor() {
+        var OboId = new OboId(OBO_ID);
+        assertThat(OboId.id()).isEqualTo(OBO_ID);
+    }
+
+    @Test
+    public void shouldThrowNpeIfLexicalValueIsNull() {
+        // Act and Assert
+        assertThatThrownBy(() -> new OboId(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void shouldCreateOboIdViaFactoryMethod() {
+        var lexicalValue = "http://example.com/OboId";
+        var oboId = OboId.valueOf(lexicalValue);
+        assertThat(oboId).isNotNull();
+        assertThat(oboId.id()).isEqualTo(lexicalValue);
+    }
+
+    @SuppressWarnings("DataFlowIssue")
+    @Test
+    public void shouldThrowNpeIfNullIsPassedToFactoryMethod() {
+        // Act and Assert
+        assertThatThrownBy(() -> OboId.valueOf(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void shouldDeserializeJson() throws IOException {
+        var read = tester.readObject(new StringReader("""
+                                  "GO:1234567"
+                                  """));
+
+        assertThat(read.id()).isEqualTo(OBO_ID);
+    }
+
+    @Test
+    void shouldSerializeJson() throws IOException {
+        var content = tester.write(OboId.valueOf(OBO_ID));
+        assertThat(content.getJson()).isEqualTo("\"GO:1234567\"");
+    }
+}
